c0c9b03a8c7fef22843b4d2d260d84611168cbdb
Augustus Kling <augustus.kling@gmail.com>
2020-03-21 21:28:24 +0100

    Add Breigau-Hochschwarzwald, Freiburg, Ortenaukreis

---
+++ src/scrapers/breisgau-hochschwarzwald.ts
@@ -0,0 +1,18 @@
+import { Scraper } from '../scraper';
+import * as moment from 'moment-timezone';
+
+class ScraperImpl extends Scraper {
+    public async get() {
+        const matches = await this.downloadAndMatch(
+            'https://www.breisgau-hochschwarzwald.de/pb/Breisgau-Hochschwarzwald/Start/Service+_+Verwaltung/Corona-Virus.html',
+            /<tr>\s*<td><strong>Stand<\/strong><\/td>\s*<td><strong>Fallzahlen gesamt<\/strong><\/td>\s*<td><strong>Stadt Freiburg<\/strong><\/td>\s*<td><strong>LK Breisgau-Hochschwarzwald<\/strong><\/td>\s*<\/tr>\s*<tr>\s*<td>(.+?) Uhr<\/td>\s*<td><span[^>]*>\d+<\/span><\/td>\s*<td>\d+<\/td>\s*<td>(\d+)<\/td>\s*<\/tr>/
+        );
+        return {
+            NUTS: 'DE132',
+            cumulatedInfected: parseInt(matches[2], 10),
+            updateDate: moment.tz(matches[1], 'DD.MM., HH:mm', 'Europe/Berlin').toISOString()
+        };
+    }
+}
+
+export const scraper = new ScraperImpl();
---
+++ src/scrapers/ortenaukreis.ts
@@ -0,0 +1,16 @@
+import { Scraper } from '../scraper';
+
+class ScraperImpl extends Scraper {
+    public async get() {
+        const matches = await this.downloadAndMatch(
+            'https://www.ortenaukreis.de/corona',
+            /Zahl der best&auml;tigten Corona-Infizierten(?: im Ortenaukreis)?(?: steigt)? auf (\d+)/
+        );
+        return {
+            NUTS: 'DE134',
+            cumulatedInfected: parseInt(matches[1], 10)
+        };
+    }
+}
+
+export const scraper = new ScraperImpl();
