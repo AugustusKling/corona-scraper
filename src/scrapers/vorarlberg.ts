753f48ad8ab2590a604ed2c6c5d544445e59541b
Augustus Kling <augustus.kling@gmail.com>
2020-03-21 23:46:11 +0100

    Add Vorarlberg

---
+++ src/scrapers/vorarlberg.ts
@@ -0,0 +1,16 @@
+import { Scraper } from '../scraper';
+
+class ScraperImpl extends Scraper {
+    public async get() {
+        const matches = await this.downloadAndMatch(
+            'https://vorarlberg.at/web/land-vorarlberg/contentdetailseite/-/asset_publisher/qA6AJ38txu0k/content/informationen-zum-coronavirus?article_id=554628',
+            /In Vorarlberg sind bisher \(.+?\) gesamt \d+ Verdachtsf&auml;lle aufgetreten, davon wurden \d+ negativ getestet, (\d+) positiv/
+        );
+        return {
+            NUTS: 'AT342',
+            cumulatedInfected: parseInt(matches[1], 10)
+        };
+    }
+}
+
+export const scraper = new ScraperImpl();
