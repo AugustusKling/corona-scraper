d70c557a286b9ad641156c99ae2cc64c0fc22e3a
Augustus Kling <augustus.kling@gmail.com>
2020-03-21 19:12:29 +0100

    Add Heidelberg and Rhein-Neckar-Kreis

---
+++ src/scrapers/heidelberg.ts
@@ -0,0 +1,18 @@
+import { Scraper } from '../scraper';
+import * as moment from 'moment-timezone';
+
+class Heidelberg extends Scraper {
+    public async get() {
+        const matches = await this.downloadAndMatch(
+            'https://www.rhein-neckar-kreis.de/start/landratsamt/coronavirus+-+faq.html',
+            /Die Zahl der positiv getesteten Personen beträgt nun \d+\. Davon\s+\d+ im Rhein\-Neckar\-Kreis und\s+(\d+) im Stadtgebiet Heidelberg <i\>\(Stand: (.+?)\)/
+        );
+        return {
+            NUTS: 'DE125',
+            cumulatedInfected: parseInt(matches[1], 10),
+            updateDate: moment.tz(matches[2], 'DD. MMMM YYYY', 'de', 'Europe/Berlin').format('YYYY-MM-DD')
+        };
+    }
+}
+
+export const scraper = new Heidelberg();
