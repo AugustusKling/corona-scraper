657943ae100e07432387fbb13e8fe39a764699f1
Augustus Kling <augustus.kling@gmail.com>
2020-03-22 22:01:20 +0100

    Fix Karlsruhe

---
+++ src/scrapers/karlsruhe.ts
@@ -0,0 +1,21 @@
+import { Scraper } from '../scraper';
+
+class KarlsruheLandkreis extends Scraper {
+    public async get() {
+        const matches = await this.downloadAndMatch(
+            'https://corona.karlsruhe.de/',
+            /Stadt Karlsruhe: (\d+),[^]*?davon \d+ aus Quarantäne entlassen[^]+?Landkreis Karlsruhe: (\d+),[^]*?davon \d+ aus Quarantäne entlassen/
+        );
+        return [
+            {
+                NUTS: 'DE122',
+                cumulatedInfected: parseInt(matches[1], 10)
+            },{
+                NUTS: 'DE123',
+                cumulatedInfected: parseInt(matches[2], 10)
+            }
+        ];
+    }
+}
+
+export const scraper = new KarlsruheLandkreis();
