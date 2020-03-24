bdbfe38162add6dff2faff8a0e6f6bdfca3e0f06
Augustus Kling <augustus.kling@gmail.com>
2020-03-24 20:19:09 +0100

    Fix Zollernalbkreis

--- src/scrapers/zollernalbkreis.ts
+++ src/scrapers/zollernalbkreis.ts
@@ -5,12 +5,12 @@ class Zollernalbkreis extends Scraper {
     public async get() {
         const matches = await this.downloadAndMatch(
             'https://www.zollernalbkreis.de/aktuelles/nachrichten/antworten+auf+haeufig+gestellte+fragen+zum+neuartigen+coronavirus',
-            /Gesamtzahl der an dem Coronavirus-Infizierten im Zollernalbkreis: <strong>(\d+)\s*<\/strong>\(Stand: (.+) Uhr\)/
+            /Gesamtzahl der an dem Coronavirus-Infizierten im Zollernalbkreis: (?:<strong>)?(\d+).*\(Stand: (.+) Uhr\)/
         );
         return {
             NUTS: 'DE143',
             cumulatedInfected: parseInt(matches[1], 10),
-            updateDate: moment.tz(matches[2], 'D.M.YYYY, H', 'Europe/Berlin').toISOString()
+            updateDate: moment.tz(matches[2], 'D.M.YYYY, H.m', 'Europe/Berlin').toISOString()
         };
     }
 }
