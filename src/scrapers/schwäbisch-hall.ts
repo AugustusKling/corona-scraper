5856327bd71ab6c722dd6b2bd86015a8c5f0e20c
Augustus Kling <augustus.kling@gmail.com>
2020-03-24 20:06:09 +0100

    Fix Schwäbisch Hall

--- src/scrapers/schwäbisch-hall.ts
+++ src/scrapers/schwäbisch-hall.ts
@@ -5,7 +5,7 @@ class SchwäbischHall extends Scraper {
     public async get() {
         const matches = await this.downloadAndMatch(
             'https://www.lrasha.de/index.php?id=953?&no_cache=1&publish[id]=1107342&publish[start]=',
-            /Stand: \w+, (.+) Uhr[^]+Im Landkreis Schw&auml;bisch Hall haben haben wir aktuell (\d+)&nbsp;best&auml;tigte Corona-Erkrankte./
+            /Stand: \w+, (.+)(?:&nbsp;|\s)+Uhr[^]+Im Landkreis Schw&auml;bisch Hall haben(?:&nbsp;|\s)+wir aktuell(?:&nbsp;|\s+)<strong>(\d+)<\/strong>&nbsp;best&auml;tigte Corona-Erkrankte/
         );
         return {
             NUTS: 'DE11A',
