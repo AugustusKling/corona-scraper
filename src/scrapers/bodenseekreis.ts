4d2010f760eb691ddf554bea94e8e7e1760ae0c5
Augustus Kling <augustus.kling@gmail.com>
2020-03-24 18:35:31 +0100

    Fix Bodenseekreis

--- src/scrapers/bodenseekreis.ts
+++ src/scrapers/bodenseekreis.ts
@@ -4,12 +4,14 @@ class Bodenseekreis extends Scraper {
     public async get() {
         const matches = await this.downloadAndMatch(
             'https://www.bodenseekreis.de/de/soziales-gesundheit/gesundheit/infektionsschutz/infektionskrankheiten/corona-virus/',
-            /labordiagnostisch bestätigte Infektionen:&nbsp;(\d+) Personen[^]+in behördlich angeordneter häuslicher Quarantäne: circa&nbsp;(\d+) Personen/
+            /Labordiagnostisch bestätigte Infektionsfälle seit Ausbruch: <\/b>(\d+) \((\d+) gelten zwischenzeitlich als genesen\)[^]*?Personen in stationärer Behandlung: <\/b>(\d+)[^]*?Personen in behördlich angeordneter häuslicher Quarantäne: <\/b>rund (\d+)/
         );
         return {
             NUTS: 'DE147',
             cumulatedInfected: parseInt(matches[1], 10),
-            cumulatedQuarantined: parseInt(matches[2], 10)
+            cumulatedRecovered: parseInt(matches[2], 10),
+            currentlyHospitalized: parseInt(matches[3], 10),
+            cumulatedQuarantined: parseInt(matches[4], 10)
         };
     }
 }
