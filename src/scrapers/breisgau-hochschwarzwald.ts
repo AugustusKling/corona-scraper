6a969506eb8dd300d60072f2291bfe0e19a3bb41
Augustus Kling <augustus.kling@gmail.com>
2020-03-25 23:43:41 +0100

    Fix Breisgau-Hochschwarzwald and Freiburg

--- src/scrapers/breisgau-hochschwarzwald.ts
+++ src/scrapers/breisgau-hochschwarzwald.ts
@@ -5,20 +5,20 @@ class ScraperImpl extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
             'https://www.breisgau-hochschwarzwald.de/pb/Breisgau-Hochschwarzwald/Start/Service+_+Verwaltung/Corona-Virus.html',
-            /<tr>\s*<td><strong>Stand<\/strong><\/td>\s*<td><strong>Fallzahlen gesamt<\/strong><\/td>\s*<td><strong>Stadt Freiburg<\/strong><\/td>\s*<td><strong>LK Breisgau-Hochschwarzwald<\/strong><\/td>\s*<\/tr>\s*<tr>\s*<td>(?<updateDate>.+?) Uhr<\/td>\s*<td>\d+<\/td>\s*<td>(?<freiburg>\d+)<\/td>\s*<td>(?<breisgauHochschwarzwald>\d+)<\/td>\s*<\/tr>/
+            /<tr>\s*<td><strong>Stand<\/strong><\/td>\s*<td><strong>Fallzahlen gesamt<\/strong><\/td>\s*<td><strong>Stadt Freiburg<\/strong><\/td>\s*<td><strong>LK Breisgau-Hochschwarzwald<\/strong><\/td>\s*<\/tr>\s*<tr>\s*<td>(?<updateDate>.+?) Uhr.*?<\/td>\s*<td>\d+<\/td>\s*<td>(?<freiburg>\d+)<\/td>\s*<td>(?<breisgauHochschwarzwald>\d+)<\/td>\s*<\/tr>/
         );
         const updateDate = moment.tz(groups.updateDate, 'DD.MM., HH:mm', 'Europe/Berlin').toISOString()
         return [
             {
                 // Freiburg
                 NUTS: 'DE131',
-                cumulatedInfected: this.parseNumber(groups.freiburg),
+                cumulatedInfected: groups.freiburg,
                 updateDate
             },
             {
                 // Breisgau-Hochschwarzwald
                 NUTS: 'DE132',
-                cumulatedInfected: this.parseNumber(groups.breisgauHochschwarzwald),
+                cumulatedInfected: groups.breisgauHochschwarzwald,
                 updateDate
             }
         ];
