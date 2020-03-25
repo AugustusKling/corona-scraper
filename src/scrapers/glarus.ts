00a67831923e6c0624998c9a9c0136bce71f928d
Augustus Kling <augustus.kling@gmail.com>
2020-03-25 19:14:18 +0100

    Add Glarus

---
+++ src/scrapers/glarus.ts
@@ -0,0 +1,19 @@
+import { Scraper } from '../scraper';
+import * as moment from 'moment-timezone';
+
+class ScraperImpl extends Scraper {
+    public async get() {
+        const { groups } = await this.downloadAndMatch(
+            'https://www.gl.ch/verwaltung/finanzen-und-gesundheit/gesundheit/coronavirus.html/4817',
+            /Update Kanton Glarus.+?\(Stand: (?<updateDate>.+?) Uhr\)[^]*Bestätigte Fälle: <strong>\s*(?<cumulatedInfected>\d+)\s*<\/strong>.+?Hospitalisierungen: <strong>\s*(?<cumulatedHospitalized>\d+)\s*<\/strong>/
+        );
+        return {
+            NUTS: 'CH051',
+            updateDate: moment.tz(groups.updateDate, 'D.M.YYYY, HH:mm', 'de', 'Europe/Berlin').toISOString(),
+            cumulatedInfected: this.parseNumber(groups.cumulatedInfected),
+            cumulatedHospitalized: this.parseNumber(groups.cumulatedHospitalized)
+        };
+    }
+}
+
+export const scraper = new ScraperImpl();
