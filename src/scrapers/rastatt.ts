f7bf82e503b79a091e98de25d194709b960d14b6
Augustus Kling <augustus.kling@gmail.com>
2020-03-26 21:47:56 +0100

    Fix Rhein-Neckar-Kreis and Heidelberg

--- src/scrapers/rastatt.ts
+++ src/scrapers/rastatt.ts
@@ -5,7 +5,7 @@ class Rastatt extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
             'https://www.baden-baden.de/stadtportrait/aktuelles/themen/coronavirus/corona-aktuell/',
-            /In Baden-Baden sind inzwischen (?<cumulatedInfectedBadenBaden>\d+) Personen infiziert, im Landkreis Rastatt (?<cumulatedInfectedRastatt>\d+) Personen. Zusammen sind das \d+ Personen im Zuständigkeitsbereich des Gesundheitsamts Rastatt. Insgesamt sind \w+ Corona-Patienten genesen und aus der Quarantäne entlassen. \d+ aktive COVID-19-Fälle befinden sich in Quarantäne. \(Stand (?<updateDate>.{1,40}) Uhr\)/
+            /In Baden-Baden sind inzwischen (?<cumulatedInfectedBadenBaden>\d+) Personen infiziert, im Landkreis Rastatt (?<cumulatedInfectedRastatt>\d+) Personen. Zusammen sind das \d+ Personen im Zuständigkeitsbereich des Gesundheitsamts Rastatt. Insgesamt sind \w+ Corona-Patienten genesen und aus der Quarantäne entlassen. \d+ aktive COVID-19-Fälle befinden sich in Quarantäne. \(Stand (?<updateDate>.{1,40}?) Uhr\)/
         );
         
         const updateDate = moment.tz(groups.updateDate, 'DD. MMM, HH', 'de', 'Europe/Berlin').toISOString();
--- src/scrapers/rhein-neckar-kreis.ts
+++ src/scrapers/rhein-neckar-kreis.ts
@@ -1,24 +1,24 @@
-import { Scraper } from '../scraper';
+import { Scraper, parseNumber } from '../scraper';
 import * as moment from 'moment-timezone';
 
 class RheinNeckarKreis extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
             'https://www.rhein-neckar-kreis.de/,Lde/start/landratsamt/coronavirus+-+faq.html',
-            /Die Zahl der positiv getesteten Personen beträgt nun \d+\. Davon\s+(?<rheinNeckar>\d+) im Rhein\-Neckar\-Kreis und\s+(?<heidelberg>\d+) im Stadtgebiet Heidelberg.+?\(Stand: (?<updateDate>.+?)\)/
+            /Im Rhein-Neckar-Kreis beträgt die Zahl der positiv getesteten Personen (?<rheinNeckar>\d+); im Stadtgebiet Heidelberg (?<heidelberg>\d+)\. <i>\(Stand: (?<updateDate>.{1,40}?)\)/
         );
         const updateDate = moment.tz(groups.updateDate, 'DD. MMM YYYY', 'de', 'Europe/Berlin').format('YYYY-MM-DD');
         return [
             {
                 // Heidelberg
                 NUTS: 'DE125',
-                cumulatedInfected: this.parseNumber(groups.heidelberg),
+                cumulatedInfected: parseNumber(groups.heidelberg),
                 updateDate
             },
             {
                 // Rhein-Neckar-Kreis
                 NUTS: 'DE128',
-                cumulatedInfected: this.parseNumber(groups.rheinNeckar),
+                cumulatedInfected: parseNumber(groups.rheinNeckar),
                 updateDate
             }
         ];
