967172cc3493cef917e911cca15e107d8e2f2d5b
Augustus Kling <augustus.kling@gmail.com>
2020-03-26 22:34:25 +0100

    Add Garmisch-Partenkirchen

---
+++ src/scrapers/garmisch-partenkirchen.ts
@@ -0,0 +1,18 @@
+import { Scraper } from '../scraper';
+import * as moment from 'moment-timezone';
+
+class ScraperImpl extends Scraper {
+    public async get() {
+        const { groups } = await this.downloadAndMatch(
+            'https://www.lra-gap.de/de/coronavirus.html',
+            /im Landkreis Garmisch-Partenkirchen <strong>(?<cumulatedInfected>\d+)<\/strong> nachweislich Corona-Erkrankte \(Stand (?<updateDate>.{10,40}) Uhr\)/
+        );
+        return {
+            ...groups,
+            NUTS: 'DE21D',
+            updateDate: moment.tz(groups.updateDate, 'DD.MM.YYYY, HH.mm', 'de', 'Europe/Berlin').toISOString()
+        };
+    }
+}
+
+export const scraper = new ScraperImpl();
