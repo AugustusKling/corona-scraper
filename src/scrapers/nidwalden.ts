94afe283ec171306ea06f2db25ddc8b8926c1da2
Augustus Kling <augustus.kling@gmail.com>
2020-03-27 12:41:00 +0100

    Fix Nidwalden

--- src/scrapers/nidwalden.ts
+++ src/scrapers/nidwalden.ts
@@ -1,14 +1,16 @@
 import { Scraper } from '../scraper';
+import * as moment from 'moment-timezone';
 
 class ScraperImpl extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
             'https://www.nw.ch/gesundheitsamtdienste/6044',
-            /Bisher sind (?<cumulatedInfected>\d+) Personen im Kanton Nidwalden positiv auf das Coronavirus getestet worden/
+            /Stand: (?<updateDate>.{5,40}) Uhr[^]+Positiv getestete Personen: (?<cumulatedInfected>\d+)<br \/>\s*Am Virus verstorbene Personen: (?<cumulatedDeaths>\d+)/
         );
         return {
+            ...groups,
             NUTS: 'CH065',
-            cumulatedInfected: this.parseNumber(groups.cumulatedInfected)
+            updateDate: moment.tz(groups.updateDate, 'DD. MMM YYYY, HH.mm', 'de', 'Europe/Berlin').toISOString()
         };
     }
 }
