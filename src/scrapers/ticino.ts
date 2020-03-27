958ff031353ff0cdae60965d91ce766f7ba466c6
Augustus Kling <augustus.kling@gmail.com>
2020-03-27 19:55:52 +0100

    Fix Ticino

--- src/scrapers/ticino.ts
+++ src/scrapers/ticino.ts
@@ -3,14 +3,14 @@ import * as moment from 'moment-timezone';
 
 class ScraperImpl extends Scraper {
     public async get() {
-        const matches = await this.downloadAndMatch(
+        const { groups } = await this.downloadAndMatch(
             'https://www4.ti.ch/dss/dsp/covid19/home/',
-            />([\d']+)<\/p><p[^>]*>Casi positivi COVID-19<\/p><p[^>]*>([\d']+)<\/p><p[^>]*>† Decessi/
+            />(?<cumulatedInfected>[\d']+)<\/p><p[^>]*>Casi positivi COVID-19<\/p><p[^>]*>(?<cumulatedDeaths>[\d']+)<\/p><p[^>]*>Decessi<\/p><\/div><\/a><h4>Stato complessivo al:<\/h4><h4>(?<updateDate>[^<]+?)<\/h4>/
         );
         return {
+            ...groups,
             NUTS: 'CH070',
-            cumulatedInfected: this.parseNumber(matches[1]),
-            cumulatedDeaths: this.parseNumber(matches[2])
+            updateDate: moment.tz(groups.updateDate, 'DD MMM YYYY[, ore ]H.mm', 'it', 'Europe/Berlin').toISOString()
         };
     }
 }
