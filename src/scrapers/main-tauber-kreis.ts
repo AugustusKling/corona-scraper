d8bbcbf5b4b240992411c0a20bb0cbdae1733a8e
Augustus Kling <augustus.kling@gmail.com>
2020-03-27 19:16:49 +0100

    Fix Main-Tauber-Kreis

--- src/scrapers/main-tauber-kreis.ts
+++ src/scrapers/main-tauber-kreis.ts
@@ -3,13 +3,14 @@ import * as moment from 'moment-timezone';
 
 class MainTauberKreis extends Scraper {
     public async get() {
-        const matches = await this.downloadAndMatch(
+        const { groups }= await this.downloadAndMatch(
             'https://www.main-tauber-kreis.de/Landratsamt/Aktuelles/Pressemitteilungen',
-            /Coronavirus-Infektion neu bestätigt. Damit steigt die Gesamtzahl auf (\d+)/
+            /Im Main-Tauber-Kreis wurden am \w+, (?<updateDate>[^,]{5,20}), \S+ neue Fälle einer Coronavirus-Infektion bestätigt. Damit liegt die Gesamtzahl der bislang infizierten Personen bei (?<cumulatedInfected>\d+)\./
         );
         return {
+            ...groups,
             NUTS: 'DE11B',
-            cumulatedInfected: parseInt(matches[1], 10)
+            updateDate: moment.tz(groups.updateDate, 'DD. MMM', 'de', 'Europe/Berlin').format('YYYY-MM-DD')
         };
     }
 }
