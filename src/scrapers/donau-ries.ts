a1f2ead4fd9ced04fba727c65418e15f60088618
Augustus Kling <augustus.kling@gmail.com>
2020-03-28 20:22:09 +0100

    Add Landkreis Donau-Ries

---
+++ src/scrapers/donau-ries.ts
@@ -0,0 +1,18 @@
+import { Scraper } from '../scraper';
+import * as moment from 'moment-timezone';
+
+class ScraperImpl extends Scraper {
+    public async get() {
+        const { groups } = await this.downloadAndMatch(
+            'https://www.donau-ries.de/Landratsamt/Buergerservice/Aufgabenbereiche/Gesundheitsamt-Humanmedizin.aspx',
+            /(?<updateDate>\d\d\.\d\d\.\d\d\d\d \d\d:\d\d) Uhr[^+]+?Das Gesundheitsamt des Landratsamtes Donau-Ries meldet mittlerweile\s+(?<cumulatedInfected>\d+)\s*bestätigte Erkrankte/
+        );
+        return {
+            ...groups,
+            NUTS: 'DE27D',
+            updateDate: moment.tz(groups.updateDate, 'DD.MM.YYYY HH:mm', 'de', 'Europe/Berlin').toISOString()
+        };
+    }
+}
+
+export const scraper = new ScraperImpl();
