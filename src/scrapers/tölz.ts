05111006d97cff2718405186dacb4c0cf8daaac6
Augustus Kling <augustus.kling@gmail.com>
2020-03-28 19:33:51 +0100

    Fix Tölz

--- src/scrapers/tölz.ts
+++ src/scrapers/tölz.ts
@@ -5,12 +5,12 @@ class ScraperImpl extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
             'https://www.lra-toelz.de/coronavirus',
-            /Im Landkreis werden heute Nachmittag, \w+, (?<updateDate>\d\d\. \S+ \d\d\d\d) insgesamt (?<cumulatedInfected>\d+) mit Covid-19 infizierte Personen gezählt/
+            /(?:Im Landkreis werden heute Nachmittag, \w+, (?<updateDate1>\d\d\. \S+ \d\d\d\d) insgesamt (?<cumulatedInfected1>\d+) mit Covid-19 infizierte Personen gezählt)|(?:Im Landkreis Bad Tölz-Wolfratshausen ist bei insgesamt (?<cumulatedInfected2>\d+) Personen das Coronavirus nachgewiesen worden. Das ist der Stand vom heutigen \w+, (?<updateDate2>\d\d\. \S+ \d\d\d\d))|(?:Mit Stand \w+, (?<updateDate3>\d\d\. \S+ \d\d\d\d), sind im Landkreis Bad Tölz-Wolfratshausen (?<cumulatedInfected3>\d+) nachgewiesene Coronafälle gezählt worden)/
         );
         return {
-            ...groups,
             NUTS: 'DE216',
-            updateDate: moment.tz(groups.updateDate, 'DD. MMM YYYY', 'de', 'Europe/Berlin').format('YYYY-MM-DD')
+            updateDate: moment.tz(groups.updateDate1 || groups.updateDate2 || groups.updateDate3, 'DD. MMM YYYY', 'de', 'Europe/Berlin').format('YYYY-MM-DD'),
+            cumulatedInfected: groups.cumulatedInfected1 || groups.cumulatedInfected2 || groups.cumulatedInfected3
         };
     }
 }
