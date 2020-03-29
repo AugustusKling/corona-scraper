3413ef6a23ddbf621a96875145ca3cf149e0ee83
Augustus Kling <augustus.kling@gmail.com>
2020-03-30 00:51:45 +0200

    Fix Vorarlberg

--- src/scrapers/vorarlberg.ts
+++ src/scrapers/vorarlberg.ts
@@ -1,14 +1,16 @@
 import { Scraper } from '../scraper';
+import * as moment from 'moment-timezone';
 
 class ScraperImpl extends Scraper {
     public async get() {
-        const matches = await this.downloadAndMatch(
+        const { groups } = await this.downloadAndMatch(
             'https://vorarlberg.at/web/land-vorarlberg/contentdetailseite/-/asset_publisher/qA6AJ38txu0k/content/informationen-zum-coronavirus?article_id=554628',
-            /Insgesamt (\d+) Personen wurden positiv getestet/
+            /Mit Stand \w+ \((?<updateDate>[^<]+?) Uhr sind in Vorarlberg (?<cumulatedInfected>\d+) Personen positiv auf das Coronavirus getestet worden/
         );
         return {
+            ...groups,
             NUTS: 'AT34',
-            cumulatedInfected: parseInt(matches[1], 10)
+            updateDate: moment.tz(groups.updateDate, 'DD. MMM YYYY[) ]HH:mm', 'de', 'Europe/Berlin').toISOString()
         };
     }
 }
