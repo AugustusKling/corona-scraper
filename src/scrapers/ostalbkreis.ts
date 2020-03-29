e799ab3e4d96ea1619170b6f4d256785fab09ee2
Augustus Kling <augustus.kling@gmail.com>
2020-03-30 00:34:43 +0200

    Fix Ostalbkreis

--- src/scrapers/ostalbkreis.ts
+++ src/scrapers/ostalbkreis.ts
@@ -3,14 +3,14 @@ import * as moment from 'moment-timezone';
 
 class Ostalbkreis extends Scraper {
     public async get() {
-        const matches = await this.downloadAndMatch(
+        const { groups } = await this.downloadAndMatch(
             'https://www.ostalbkreis.de/sixcms/detail.php?_topnav=36&_sub1=31788&_sub2=32062&_sub3=292448&id=292450',
-            /<table border="1"><tr><td align="center" width="270"><b>Corona-Erkrankte im Ostalbkreis<\/b><\/td><td align="center" width="200"><b>Stand<\/b><\/td><\/tr>[^]*<tr><td align="center">(\d+)<\/td><td align="center">(.+)<\/td><\/tr>/
+            /Corona-Erkrankte im Ostalbkreis<\/b><td[^>]*><b>Aus Isolation Entlassene<\/b><td[^>]*><b>Aktive Fälle<\/b><\/td><td[^>]*><b>Stand<\/b><\/td><\/tr>\s*<tr><td[^>]*>(?<cumulatedInfected>\d+)<\/td><td[^>]*>(?<cumulatedRecovered>\d+)<\/td><td[^>]*>(?<currentlyInfected>\d+)<\/td><td[^>]*>(?<updateDate>[^<]+)<\/td>/
         );
         return {
+            ...groups,
             NUTS: 'DE11D',
-            cumulatedInfected: parseInt(matches[1], 10),
-            updateDate: moment.tz(matches[2], 'DD.MM.YYYY', 'Europe/Berlin').toISOString()
+            updateDate: moment.tz(groups.updateDate, 'DD.MM.YYYY', 'de', 'Europe/Berlin').toISOString()
         };
     }
 }
