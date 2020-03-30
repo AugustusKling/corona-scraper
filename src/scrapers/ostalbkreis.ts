98ec71e2f9aed462aabd821a3cac4f2c4c64de39
Augustus Kling <augustus.kling@gmail.com>
2020-03-30 13:49:06 +0200

    Fix Ostalbkreis

--- src/scrapers/ostalbkreis.ts
+++ src/scrapers/ostalbkreis.ts
@@ -5,7 +5,7 @@ class Ostalbkreis extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
             'https://www.ostalbkreis.de/sixcms/detail.php?_topnav=36&_sub1=31788&_sub2=32062&_sub3=292448&id=292450',
-            /Corona-Erkrankte im Ostalbkreis<\/b><td[^>]*><b>Aus Isolation Entlassene<\/b><td[^>]*><b>Aktive Fälle<\/b><\/td><td[^>]*><b>Stand<\/b><\/td><\/tr>\s*<tr><td[^>]*>(?<cumulatedInfected>\d+)<\/td><td[^>]*>(?<cumulatedRecovered>\d+)<\/td><td[^>]*>(?<currentlyInfected>\d+)<\/td><td[^>]*>(?<updateDate>[^<]+)<\/td>/
+            /Corona-Erkrankte im Ostalbkreis<\/b><\/td><td[^>]*><b>Aus Isolation Entlassene<\/b><\/td><td[^>]*><b>Aktive Fälle<\/b><\/td><td[^>]*><b>Stand<\/b><\/td><\/tr>\s*<tr><td[^>]*>(?<cumulatedInfected>\d+)<\/td><td[^>]*>(?<cumulatedRecovered>\d+)<\/td><td[^>]*>(?<currentlyInfected>\d+)<\/td><td[^>]*>(?<updateDate>[^<]+)<\/td>/
         );
         return {
             ...groups,
