d15cfcbeef4ec2c8cefa79e354c3bdaad2229e92
Augustus Kling <augustus.kling@gmail.com>
2020-03-30 14:16:24 +0200

    Fix Ortenaukreis

--- src/scrapers/ortenaukreis.ts
+++ src/scrapers/ortenaukreis.ts
@@ -1,14 +1,16 @@
 import { Scraper } from '../scraper';
+import * as moment from 'moment-timezone';
 
 class ScraperImpl extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
             'https://www.ortenaukreis.de/corona',
-            /(?:Zahl|Fallzahl) der bestätigten Corona-Infizierten(?: im Ortenaukreis)?(?: steigt)? auf (?<cumulatedInfected>\d+)/
+            /<div class="date">(?<updateDate>[^<]+)<\/div>(?:[^](?!<div class="date">))+[>\s](?<cumulatedInfected>\d+) bestätigte Covid-19-Fälle im Ortenaukreis/
         );
         return {
             ...groups,
-            NUTS: 'DE134'
+            NUTS: 'DE134',
+            updateDate: moment.tz(groups.updateDate, 'DD.MM.YYYY', 'de', 'Europe/Berlin').format('YYYY-MM-DD')
         };
     }
 }
