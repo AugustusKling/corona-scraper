9147cf376e3b4f7e35b2fc1738371411f5ba0bf4
Augustus Kling <augustus.kling@gmail.com>
2020-03-30 19:46:40 +0200

    Fix Freudenstadt

--- src/scrapers/freudenstadt.ts
+++ src/scrapers/freudenstadt.ts
@@ -5,7 +5,7 @@ class ScraperImpl extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
             'https://www.landkreis-freudenstadt.de/Startseite/Aktuell/aktuelle+situation+im+landkreis+freudenstadt.html',
-            /Nach Auswertung der am Sonntag \((?<updateDate>[^)]+)\) eingegangenen Laborergebnisse ist die Zahl der im Landkreis Freudenstadt auf den Coronavirus positiv getesteten Personen auf (?<cumulatedInfected>\d+) gestiegen/
+            /Die Zahl der im Landkreis Freudenstadt auf den Coronavirus positiv getesteten Personen ist am \w+ \((?<updateDate>[^)]+)\) auf (?<cumulatedInfected>\d+) gestiegen/
         );
         return {
             ...groups,
