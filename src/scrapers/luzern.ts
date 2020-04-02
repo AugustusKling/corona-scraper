bb49be687b91ba54dcb0726356bd96efc59b32ac
Augustus Kling <augustus.kling@gmail.com>
2020-04-02 13:26:36 +0200

    Fix Luzern

--- src/scrapers/luzern.ts
+++ src/scrapers/luzern.ts
@@ -5,12 +5,12 @@ class ScraperImpl extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
             'https://gesundheit.lu.ch/themen/Humanmedizin/Infektionskrankheiten/Coronavirus',
-            /Im Kanton Luzern gibt es (?<cumulatedInfected>\d+) bestätige Fälle \(Stand: (?<updateDate>.+?) Uhr\)/
+            /Aktuelle Fallzahlen im Kanton Luzern\s*<\/strong>\(Stand: (?<updateDate>[^)]+) Uhr\)[^]*<tr(?:[^](?!<\/tr>))+Bestätigte Fälle:(?:[^](?!<\/tr>))+>(?<cumulatedInfected>\d+)<\/p>[^]*<tr(?:[^](?!<\/tr>))+Hospitalisiert:(?:[^](?!<\/tr>))+>(?<currentlyHospitalized>\d+)<\/p>[^]*<tr(?:[^](?!<\/tr>))+Intensivpflege:(?:[^](?!<\/tr>))+>(?<currentlyIntensiveCare>\d+)<\/p>[^]*<tr(?:[^](?!<\/tr>))+Todesfälle:(?:[^](?!<\/tr>))+>(?<cumulatedDeaths>\d+)<\/p>/
         );
         return {
+            ...groups,
             NUTS: 'CH061',
-            updateDate: moment.tz(groups.updateDate, 'DD. MMM YYYY, HH:mm', 'de', 'Europe/Berlin').toISOString(),
-            cumulatedInfected: this.parseNumber(groups.cumulatedInfected)
+            updateDate: moment.tz(groups.updateDate, 'D. MMM YYYY, HH:mm', 'de', 'Europe/Berlin').toISOString()
         };
     }
 }
