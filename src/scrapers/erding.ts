e731f8490d876e71615129b3f0b3c8bc04779e8a
Augustus Kling <augustus.kling@gmail.com>
2020-04-02 12:07:17 +0200

    Fix Erding

--- src/scrapers/erding.ts
+++ src/scrapers/erding.ts
@@ -4,12 +4,12 @@ import * as moment from 'moment-timezone';
 class ScraperImpl extends Scraper {
     public async get() {
         const pressrelease = await this.downloadAndMatch(
-            'https://www.landkreis-erding.de/gesundheitswesen/gesundheitsamt/',
-            /<a href="(?<url>[^"]*)"[^>]*>Corona-Virus im Landkreis Erding – Sachstand \d\d\.\d\d\.\d\d\d\d<\/a>/
+            'https://www.landkreis-erding.de/aktuell/pressemitteilungen/?q=Corona&start=&end=',
+            /<a href="(?<url>\/aktuell\/pressemitteilungen\/corona-virus-im-landkreis-erding-sachstand-(?<updateDate>\d{8})[^"]*)"/
         );
         const { groups } = await this.downloadAndMatch(
             'https://www.landkreis-erding.de' + pressrelease.groups.url,
-            /<span property="s:dateModified">(?<updateDate>[^>]+)<\/span>[^]+?Zahl der bestätigten Covid-19-Fälle im Landkreis Erding von \d+ auf (?<cumulatedInfected>\d+) Fälle angewachsen[^]+?Im Klinikum Landkreis Erding liegen derzeit (?<currentlyHospitalized>\d+) Covid-19-Patienten; \d+ werden auf der Isolierstation behandelt, (?<currentlyIntensiveCare>\d+) auf der Intensiv-Isolierstation/
+            /<h1[^>]*>[^<]*Sachstand (?<updateDate>\d\d\.\d\d\.\d\d\d\d)[^<]*<\/h1>[^]+?Zahl der bestätigten Covid-19-Fälle im Landkreis Erding von \d+ auf (?<cumulatedInfected>\d+) Fälle angewachsen[^]+?Im Klinikum Landkreis Erding liegen derzeit (?<currentlyHospitalized>\d+) Covid-19-Patienten; \d+ werden auf der Isolierstation behandelt, (?<currentlyIntensiveCare>\d+) auf der Intensiv-Isolierstation/
         );
         return {
             ...groups,
