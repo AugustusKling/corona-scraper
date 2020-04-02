2b81e3041b6070f1d03588da730c93a98ffac638
Augustus Kling <augustus.kling@gmail.com>
2020-04-02 13:12:15 +0200

    Fix Konstanz

--- src/scrapers/konstanz.ts
+++ src/scrapers/konstanz.ts
@@ -1,14 +1,16 @@
 import { Scraper } from '../scraper';
+import * as moment from 'moment-timezone';
 
 class Konstanz extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
             'https://www.lrakn.de/,Lde/service-und-verwaltung/aemter/gesundheit+und+versorgung/coronavirus',
-            /Nach dieser Zählweise steigt die Zahl der erkrankten Personen auf (?<cumulatedInfected>\d+)\.[^]+(?<cumulatedRecovered>\d+) Personen gelten als genesen/
+            /Übersicht der aktuellen Lage im Landkreis Konstanz \(Stand \w+,  (?<updateDate>\d\d\.\d\d\.\d\d\d\d)\)[^]+?<td><strong>Gesamtanzahl Infizierte<\/strong><\/td>\s*<td><strong>(?<cumulatedInfected>\d+)<\/strong><\/td>[^]+?genesene Personen<\/td>\s*<td>(?<cumulatedRecovered>\d+)[^]+?in stationärer Behandlung<\/td>\s*<td>(?<currentlyHospitalized>\d+)<\/td>[^]+?verstorbene Personen<\/td>\s*<td>(?<cumulatedDeaths>\d+)<\/td>/
         );
         return {
             ...groups,
-            NUTS: 'DE138'
+            NUTS: 'DE138',
+            updateDate: moment.tz(groups.updateDate, 'DD.MM.YYYY', 'de', 'Europe/Berlin').toISOString()
         };
     }
 }
