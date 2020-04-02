8be15cea63de0985f984a585aa32726cb8980a37
Augustus Kling <augustus.kling@gmail.com>
2020-04-02 13:43:29 +0200

    Fix Stadt München

--- src/scrapers/stadt-münchen.ts
+++ src/scrapers/stadt-münchen.ts
@@ -4,13 +4,13 @@ import * as moment from 'moment-timezone';
 class ScraperImpl extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
-            'https://www.muenchen.de/rathaus/Stadtinfos/Presse-Service/Presse-Archiv/2020/Coronavirus-Aktuelle-Entwicklung.html',
-            /(?<updateDate>\d\d\. \S+ \(Stand \d\d\.\d\d) Uhr\), \d+ neue (?:Corona-)?Fälle bestätigt.+?in der Landeshauptstadt aktuell insgesamt (?<cumulatedInfected>\S+) Infektionen gemeldet/
+            'https://www.muenchen.de/rathaus/Stadtinfos/Coronavirus-Fallzahlen.html',
+            /(?<updateDate>\d{1,2}\. \S+ \d{4} \(Stand: \d{1,2}[.:]\d\d) Uhr\)[^]+?in der Landeshauptstadt aktuell insgesamt (?<cumulatedInfected>\S+) Infektionen gemeldet[^]+?(?<cumulatedRecovered>\S+) Personen, die bereits geheilt sind/
         );
         return {
             ...groups,
             NUTS: 'DE212',
-            updateDate: moment.tz(groups.updateDate, 'DD. MMM [(Stand] HH.mm', 'de', 'Europe/Berlin').toISOString()
+            updateDate: moment.tz(groups.updateDate, 'DD. MMM [(Stand:] HH.mm', 'de', 'Europe/Berlin').toISOString()
         };
     }
 }
