d90b73b09b02562159e42d41da6d329d40f88dc2
Augustus Kling <augustus.kling@gmail.com>
2020-04-02 23:04:11 +0200

    Fix Landkreis München

--- src/scrapers/landkreis-münchen.ts
+++ src/scrapers/landkreis-münchen.ts
@@ -4,8 +4,8 @@ import * as moment from 'moment-timezone';
 class ScraperImpl extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
-            'https://www.landkreis-muenchen.de/themen/verbraucherschutz-gesundheit/gesundheit/fallzahlen-nach-gemeinden/',
-            /im Landkreis München aktuell (?<cumulatedInfected>\d+) bestätigte Erkrankungsfälle \(Stand: (?<updateDate>.{10,40}) Uhr\)/
+            'https://www.landkreis-muenchen.de/themen/verbraucherschutz-gesundheit/gesundheit/coronavirus/fallzahlen/',
+            /insgesamt <strong>(?<cumulatedInfected>\d+) bestätigte Infektionsfälle<\/strong> \(Stand: (?<updateDate>.{10,40}) Uhr\)[^]+?insgesamt <strong>(?<cumulatedDeaths>\d+) Todesfälle<\/strong> bestätigt[^]+?Insgesamt gelten <strong>(?<cumulatedRecovered>\d+) Personen <\/strong>als statistisch<strong> genesen/
         );
         return {
             ...groups,
