ab34deab9b897c6351b9467123d13d61ca2dd02e
Augustus Kling <augustus.kling@gmail.com>
2020-04-02 14:00:40 +0200

    Fix Obwalden

--- src/scrapers/obwalden.ts
+++ src/scrapers/obwalden.ts
@@ -5,12 +5,12 @@ class ScraperImpl extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
             'https://www.ow.ch/de/verwaltung/dienstleistungen/?dienst_id=5962',
-            /Bisher ist bei (?<cumulatedInfected>\d+) Personen im Kanton Obwalden das Coronavirus nachgewiesen worden. Bereits genesene Personen sind in dieser Zahl ebenfalls enthalten \(Stand: (?<updateDate>.{10,40}?)\)/
+            /Bisher ist bei (?<cumulatedInfected>\d+) Personen im Kanton Obwalden das Coronavirus nachgewiesen worden. Bereits(?: wieder)? genesene Personen sind in dieser Zahl ebenfalls enthalten \(Stand: (?<updateDate>.{10,40}?)\)/
         );
         return {
             ...groups,
             NUTS: 'CH064',
-            updateDate: moment.tz(groups.updateDate.replace(/M�rz/, 'März'), 'DD. MMM YYYY', 'de', 'Europe/Berlin').format('YYYY-MM-DD'),
+            updateDate: moment.tz(groups.updateDate, 'DD. MMM YYYY', 'de', 'Europe/Berlin').format('YYYY-MM-DD'),
         };
     }
 }
