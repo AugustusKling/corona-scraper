ee669b5f67bcd6365179afa1ec5285177645ce14
Augustus Kling <augustus.kling@gmail.com>
2020-04-02 13:03:39 +0200

    Fix Jura

--- src/scrapers/jura.ts
+++ src/scrapers/jura.ts
@@ -5,12 +5,12 @@ class ScraperImpl extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
             'https://www.jura.ch/fr/Autorites/Coronavirus/Chiffres-H-JU/Evolution-des-cas-COVID-19-dans-le-Jura.html',
-            /[^]+<tr>\s*<(?:td|th)[^>]*>(?:<strong>)?(?<updateDate>\d{1,2} \w+ \d\d\d\d)(?:<\/strong>)?<\/(?:td|th)>\s*<td>(?<cumulatedInfected>\d+)<\/td>\s*<td>(?<currentlyHospitalized>\d+)<\/td>\s*<td>(?<currentlyIntensiveCare>\d+)<\/td>(?:\s*<td>(?<cumulatedDeaths>\d+)<\/td>)?/
+            /[^]+<tr>\s*<(?:td|th)[^>]*>(?:<strong>)?(?<updateDate>\d{1,2}\s*(?:<sup>[^<]*<\/sup>)?\s*\w+ \d\d\d\d)(?:<\/strong>)?<\/(?:td|th)>\s*<td>(?<cumulatedInfected>\d+)<\/td>\s*<td>(?<currentlyHospitalized>\d+)<\/td>\s*<td>(?<currentlyIntensiveCare>\d+)<\/td>(?:\s*<td>(?<cumulatedDeaths>\d+)<\/td>)?/
         );
         return {
             ...groups,
             NUTS: 'CH025',
-            updateDate: moment.tz(groups.updateDate, 'DD MMM YYYY', 'fr', 'Europe/Berlin').format('YYYY-MM-DD')
+            updateDate: moment.tz(groups.updateDate.replace(/<sup>[^<]*<\/sup>/, ' ').replace(/\s+/g, ' '), 'DD MMM YYYY', 'fr', 'Europe/Berlin').format('YYYY-MM-DD')
         };
     }
 }
