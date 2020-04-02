5f8c24d215825382445ffa3c7d8c35a1408a2046
Augustus Kling <augustus.kling@gmail.com>
2020-04-02 09:13:16 +0200

    Fix Sigmaringen

--- src/scrapers/sigmaringen.ts
+++ src/scrapers/sigmaringen.ts
@@ -1,14 +1,22 @@
+import fetch from 'node-fetch';
+import * as moment from 'moment-timezone';
 import { Scraper } from '../scraper';
 
 class Sigmaringen extends Scraper {
     public async get() {
-        const matches = await this.downloadAndMatch(
-            'https://www.landkreis-sigmaringen.de/de/Landratsamt/Kreisverwaltung/Fachbereiche/Gesundheit/Informationen-zum-neuartigen-Coronavirus',
-            /labordiagnostisch bestätigte Infektionen:\s*(?:<em>)?(\d+)/
-        );
+        const response = await fetch('https://services1.arcgis.com/e7l8Y32XFZQbVW4p/arcgis/rest/services/Coronastatistik_Extern/FeatureServer/1/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=STAND%20desc&resultOffset=0&resultRecordCount=1&cacheHint=true');
+        if (!response.ok) {
+            throw new Error('Failed to download: ' + response.statusText);
+        }
+        const responseObject = await response.json();
+    
+        const latest = responseObject.features[0].attributes;
         return {
             NUTS: 'DE149',
-            cumulatedInfected: parseInt(matches[1], 10)
+            population: latest.EW,
+            updateDate: moment(latest.STAND).toISOString(),
+            cumulatedInfected: latest.ANZ_INF,
+            cumulatedDeaths: latest.ANZ_TOD
         };
     }
 }
