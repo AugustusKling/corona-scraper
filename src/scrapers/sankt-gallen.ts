827658ceaede5e9cd11c9e5baf110029fb89250e
Augustus Kling <augustus.kling@gmail.com>
2020-04-03 13:14:33 +0200

    Fix Sankt Gallen

--- src/scrapers/sankt-gallen.ts
+++ src/scrapers/sankt-gallen.ts
@@ -1,17 +1,17 @@
-import { Scraper } from '../scraper';
+import { Scraper, parseNumber } from '../scraper';
 import * as moment from 'moment-timezone';
 
 class ScraperImpl extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
             'https://www.sg.ch/tools/informationen-coronavirus.html',
-            /(?<updateDate>\d\d\.\d\d\.\d\d\d\d):<\/p><p>Bestätigte Fälle: (?<cumulatedInfected>\d+)<br\/>Todesfälle: (?<cumulatedDeaths>\d+)/
+            /<h4>(?<updateDate>[^<]+)<\/h4>(?:[^](?!<\/h4>))+<tr[^>]*><td[^>]*>laborbestätigte Fälle \(kumuliert\)<\/td>\s*<td[^>]*>(?<cumulatedInfected>\d+)<\/td>\s*<td>[^<]*<\/td>\s*<\/tr><tr[^>]*><td[^>]*>Hospitalisationen Isolation \(aktueller Stand\)<\/td>\s*<td>(?<currentlyHospitalizedIsolated>\d+)<\/td>\s*<td>[^<]*<\/td>\s*<\/tr><tr[^>]*><td[^>]*>Hospitalisationen Intensiv \(aktueller Stand\)<\/td>\s*<td>(?<currentlyIntensiveCare>\d+)<\/td>\s*<td>[^<]*<\/td>\s*<\/tr><tr[^>]*><td[^>]*>aus Spital entlassene \(kumuliert\)<\/td>\s*<td>\d+<\/td>\s*<td>[^<]*<\/td>\s*<\/tr><tr[^>]*><td[^>]*>Verstorbene \(kumuliert\)<\/td>\s*<td>(?<cumulatedDeaths>\d+)<\/td>/
         );
         return {
+            ...groups,
             NUTS: 'CH055',
-            updateDate: moment.tz(groups.updateDate, 'DD.MM.YYYY', 'de', 'Europe/Berlin').format('YYYY-MM-DD'),
-            cumulatedInfected: this.parseNumber(groups.cumulatedInfected),
-            cumulatedDeaths: this.parseNumber(groups.cumulatedDeaths)
+            updateDate: moment.tz(groups.updateDate, 'D. MMM YYYY', 'de', 'Europe/Berlin').format('YYYY-MM-DD'),
+            currentlyHospitalized: parseNumber(groups.currentlyHospitalizedIsolated) + parseNumber(groups.currentlyIntensiveCare)
         };
     }
 }
