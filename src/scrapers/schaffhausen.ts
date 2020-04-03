136890105e7d49f7d4209e22ee18fe321b4c1edd
Augustus Kling <augustus.kling@gmail.com>
2020-04-03 13:38:32 +0200

    Fix Schaffhausen

--- src/scrapers/schaffhausen.ts
+++ src/scrapers/schaffhausen.ts
@@ -1,16 +1,28 @@
-import { Scraper } from '../scraper';
+import { Scraper, parseNumber } from '../scraper';
+import fetch from 'node-fetch';
 import * as moment from 'moment-timezone';
 
 class ScraperImpl extends Scraper {
     public async get() {
-        const matches = await this.downloadAndMatch(
-            'https://sh.ch/CMS/Webseite/Kanton-Schaffhausen/Beh-rde/Verwaltung/Departement-des-Innern/Gesundheitsamt-3209198-DE.html',
-            /Im Kanton Schaffhausen gibt es aktuell \(Stand (.{0,40}?) Uhr\)\s+(\d+)\s+bestätige Coronavirus-Fälle/
+        const response = await fetch('https://sh.ch/CMS/content.jsp?contentid=3209198&language=DE');
+        if (!response.ok) {
+            throw new Error('Failed to download: ' + response.statusText);
+        }
+        const responseObject = await response.json();
+        const html = responseObject.data_post_content
+            .replace(/&#(\d+);/g, (wholeMatch: string, group: string) => String.fromCharCode(parseInt(group, 10)))
+            .replace(/&nbsp;/g, ' ').replace(new RegExp(decodeURIComponent('%C2%A0'), 'g'), ' ')
+            .replace(/&auml;/g, 'ä')
+            .replace(/&uuml;/g, 'ü');
+        
+        const { groups } = html.match(
+            /<p class="post_text">Im Kanton Schaffhausen gibt es aktuell \(Stand (?<updateDate>[^)]+)\):\s*<\/p>\s*<p class="post_text"><strong>Anzahl Infizierte Fälle \(kumuliert\): (?<cumulatedInfected>\d+)<\/strong><\/p>\s*<p class="post_text"><strong>Anzahl Hospitalisationen Isolation \(aktuell\): (?<currentlyHospitalizedIsolated>\d+)<\/strong><\/p>\s*<p class="post_text"><strong>Anzahl Hospitalisationen Intensiv \(aktuell\): (?<currentlyIntensiveCare>\d+)<\/strong><\/p>\s*<p class="post_text"><strong>Verstorbene \(kummuliert\): (?<cumulatedDeaths>\d+)<\/strong><\/p>/
         );
         return {
+            ...groups,
             NUTS: 'CH052',
-            cumulatedInfected: parseInt(matches[2], 10),
-            updateDate: moment.tz(matches[1], 'DD.MM.YYYY, HH:mm', 'de', 'Europe/Berlin').toISOString()
+            updateDate: moment.tz(groups.updateDate, 'DD.MM.YYYY', 'de', 'Europe/Berlin').format('YYYY-MM-DD'),
+            currentlyHospitalized: parseNumber(groups.currentlyHospitalizedIsolated) + parseNumber(groups.currentlyIntensiveCare)
         };
     }
 }
