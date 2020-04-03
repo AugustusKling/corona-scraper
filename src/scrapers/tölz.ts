fdb26da005ed416b339aea008851568a64c5ae7d
Augustus Kling <augustus.kling@gmail.com>
2020-04-03 14:00:49 +0200

    Fix Tölz

--- src/scrapers/tölz.ts
+++ src/scrapers/tölz.ts
@@ -4,13 +4,13 @@ import * as moment from 'moment-timezone';
 class ScraperImpl extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
-            'https://www.lra-toelz.de/coronavirus',
-            />[^\d<]*(?:(?<cumulatedInfected1>\d+)[^<]*infiziert|infiziert[^\d<]*(?<cumulatedInfected2>\d+))[^<]*<span class="related__download_size">[^<]*<\/span><\/a><p>[^<]*(?<updateDate>\d\d\. \S+ \d\d\d\d)/i
+            'https://www.lra-toelz.de/pressemitteilungen',
+            /<li>Pressemitteilung: (?<updateDate>\d\d\.\d\d\.\d\d\d\d)<\/li>(?:[^](?!<\/a>))+>(?:(?<cumulatedInfected1>\d+) infizierte Personen[^<]+|Zahl der Infizierten steigt auf (?<cumulatedInfected2>\d+)|(?<cumulatedInfected3>\d+) mit dem Coronavirus Infizierte.+)<\/a>/
         );
         return {
             NUTS: 'DE216',
-            updateDate: moment.tz(groups.updateDate, 'DD. MMM YYYY', 'de', 'Europe/Berlin').format('YYYY-MM-DD'),
-            cumulatedInfected: groups.cumulatedInfected1 || groups.cumulatedInfected2
+            updateDate: moment.tz(groups.updateDate, 'DD.MM.YYYY', 'de', 'Europe/Berlin').format('YYYY-MM-DD'),
+            cumulatedInfected: groups.cumulatedInfected1 || groups.cumulatedInfected2 || groups.cumulatedInfected3
         };
     }
 }
