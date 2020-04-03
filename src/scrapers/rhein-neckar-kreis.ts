088106f0e7ecc0cc050c699dc6d9702614160283
Augustus Kling <augustus.kling@gmail.com>
2020-04-03 12:56:45 +0200

    Fix Rhein-Neckar-Kreis

--- src/scrapers/rhein-neckar-kreis.ts
+++ src/scrapers/rhein-neckar-kreis.ts
@@ -5,20 +5,14 @@ class RheinNeckarKreis extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
             'https://www.rhein-neckar-kreis.de/,Lde/start/landratsamt/coronavirus+-+faq.html',
-            /Im Rhein-Neckar-Kreis beträgt die Zahl der positiv getesteten Personen (?<rheinNeckar>\d+); im Stadtgebiet Heidelberg (?<heidelberg>\d+)\. <i>\(Stand: (?<updateDate>.{1,40}?)\)/
+            /Aktuell gibt es <strong>(?<cumulatedInfected>\d+)<\/strong> positiv getestete Erkrankte; <strong>(?<cumulatedRecovered>\d+)<\/strong> Personen sind mittlerweile genesen. Die Zahl der Todesfälle in Zusammenhang mit dem Coronavirus liegt im Landkreis bei <strong>(?<cumulatedDeaths>\d+)<\/strong>[^]{0,50}?\(Stand: (?<updateDate>.{1,40}?)\)/
         );
         const updateDate = moment.tz(groups.updateDate, 'DD. MMM YYYY', 'de', 'Europe/Berlin').format('YYYY-MM-DD');
         return [
             {
-                // Heidelberg
-                NUTS: 'DE125',
-                cumulatedInfected: parseNumber(groups.heidelberg),
-                updateDate
-            },
-            {
+                ...groups,
                 // Rhein-Neckar-Kreis
                 NUTS: 'DE128',
-                cumulatedInfected: parseNumber(groups.rheinNeckar),
                 updateDate
             }
         ];
