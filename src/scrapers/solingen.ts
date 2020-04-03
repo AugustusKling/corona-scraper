4f71dd7b72c4dc7bbc827723fe218d9ec96e46a6
Augustus Kling <augustus.kling@gmail.com>
2020-04-03 13:48:22 +0200

    Fix Solingen

--- src/scrapers/solingen.ts
+++ src/scrapers/solingen.ts
@@ -5,7 +5,7 @@ class ScraperImpl extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
             'https://www.solingen.de/de/inhalt/coronavirus/',
-            /Stand (?<updateDate>[^<]+<)[^]+?Aktuell gibt es in Solingen (?<cumulatedInfected>\d+) bestätigte Fälle von Infektionen mit dem Coronavirus/
+            />Stand (?<updateDate>[^<]+)<\/p>[^]+?Aktuell sind (?<currentlyInfected>\d+) Personen nachgewiesen infiziert, (?<currentlyHospitalized>\d+) Patienten werden derzeit stationär behandelt, die übrigen ambulant betreut\.<\/p><p>Insgesamt wurden seit Ausbruch der Pandemie in Solingen bisher (?<cumulatedInfected>\d+) bestätigte Fälle gemeldet, \d+ sind seit gestern neu hinzugekommen\. (?<cumulatedRecovered>\d+) Menschen sind wieder genesen\. (?<cumulatedDeaths>\d+) (?:Todesfall ist|Todesfälle sind) zu beklagen\.<\/p><p>In häuslicher Quarantäne befinden sich derzeit insgesamt (?<currentlyQuarantined>\d+) Personen/
         );
         return {
             ...groups,
