7144e16227a30ec8d2152454681d6ebd2a34b011
Augustus Kling <augustus.kling@gmail.com>
2020-04-04 11:30:57 +0200

    Fix Waldshut

--- src/scrapers/waldshut.ts
+++ src/scrapers/waldshut.ts
@@ -5,7 +5,7 @@ class ScraperImpl extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
             'https://www.landkreis-waldshut.de/aktuelles/informationen-zum-neuartigen-coronavirus/',
-            /\(Stand:? (?<updateDate>[^<]+) Uhr\)[^]+?derzeit (?<cumulatedInfected>\d+) Covid-19-Infizierte gemeldet[^]+?(?<cumulatedRecovered>\d+) Personen gelten(?: bereits)? als genesen[^]+?(?<currentlyHospitalized>\d+) Erkrankte befinden sich derzeit in stationärer Behandlung/
+            /Neu sind (?<cumulatedInfected>\d+) Personen im Landkreis positiv getestet und dem Gesundheitsamt gemeldet\. Es handelt sich um \d+ männliche und \d+ weibliche Personen\. (?<currentlyHospitalized>\d+) Erkrankte befinden sich in stationärer Behandlung.+?(?<cumulatedRecovered>\d+) Personen sind bereits wieder genesen.+?Stand: (?<updateDate>[^<]+?) Uhr/
         );
         return {
             ...groups,
