dfc5cd4487545c09a2ee907744436480ac71432c
Augustus Kling <augustus.kling@gmail.com>
2020-04-06 15:40:42 +0200

    Fix Bodenseekreis

--- src/scrapers/bodenseekreis.ts
+++ src/scrapers/bodenseekreis.ts
@@ -4,7 +4,7 @@ class Bodenseekreis extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
             'https://www.bodenseekreis.de/de/soziales-gesundheit/gesundheit/infektionsschutz/infektionskrankheiten/corona-virus/',
-            /<li><b>Gestorbene Personen:\s*<\/b>(?<cumulatedDeaths>\d+)\s*<\/li>\s*<li><b>Personen in stationärer Behandlung:\s*<\/b>\s*(?<currentlyHospitalized>\d+)\s*<\/li>\s*<li><b>Labordiagnostisch bestätigte Infektionsfälle seit Ausbruch:\s*<\/b>\s*(?<cumulatedInfected>\d+)\s*\((?<cumulatedRecovered>\d+) gelten zwischenzeitlich als genesen\)[^]+?<li><b>Personen in behördlich angeordneter häuslicher Quarantäne:\s*<\/b>(?<currentlyQuarantined>\d+)\s*,\s*(?<cumulatedReleasedFromQuarantine>\d+)\s*weitere Personen wurden bereits wieder aus der Quarantäne entlassen</
+            /<li><strong>Gestorbene Personen:\s*<\/strong>(?<cumulatedDeaths>\d+)\s*<\/li>\s*<li><b>Personen in stationärer Behandlung:\s*<\/b>\s*(?<currentlyHospitalized>\d+)\s*<\/li>\s*<li><b>Labordiagnostisch bestätigte Infektionsfälle seit Ausbruch:\s*<\/b>\s*(?<cumulatedInfected>\d+)\s*\((?<cumulatedRecovered>\d+) gelten zwischenzeitlich als genesen\)[^]+?<li><b>Personen in behördlich angeordneter häuslicher Quarantäne:\s*<\/b>(?<currentlyQuarantined>\d+)\s*,\s*(?<cumulatedReleasedFromQuarantine>\d+)\s*weitere Personen wurden bereits wieder aus der Quarantäne entlassen</
         );
         return {
             ...groups,
