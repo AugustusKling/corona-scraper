238302006e6f89068a89595c3ca9e28650d9e5a7
Augustus Kling <augustus.kling@gmail.com>
2020-04-08 10:20:58 +0200

    Fix Biberach

--- src/scrapers/biberach.ts
+++ src/scrapers/biberach.ts
@@ -5,12 +5,12 @@ class ScraperImpl extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
             'https://www.biberach.de/index.php?id=40',
-            /(?:Im Landkreis Biberach gibt es \(Stand: (?<updateDate1>[^<]+?) Uhr\) insgesamt (?<cumulatedInfected1>\d+) bestätigte Infektionen)|(?:Stand \w+, (?<updateDate2>[^<]+?) Uhr, insgesamt (?<cumulatedInfected2>\d+) positiv bestätigte Coronafälle)|(?:Stand \w+, (?<updateDate3>[^<]+?) Uhr, gibt es im Landkreis Biberach (?<cumulatedInfected3>\d+) bestätigte Coronafälle)|(?:(?<cumulatedInfected4>\d+) bestätigte Coronafälle \(Stand (?<updateDate4>[^<]+?) Uhr\)|Im Landkreis Biberach wurden aktuell \(Stand:? (?<updateDate5>[^<]+?) Uhr\) (?<cumulatedInfected5>\d+) Personen positiv auf das Coronavirus getestet)/
+            /(?:Im Landkreis Biberach gibt es \(Stand: (?<updateDate1>[^<]+?) Uhr\) insgesamt (?<cumulatedInfected1>\d+) bestätigte Infektionen)|(?:Stand \w+, (?<updateDate2>[^<]+?) Uhr, insgesamt (?<cumulatedInfected2>\d+) positiv bestätigte Coronafälle)|(?:Stand \w+, (?<updateDate3>[^<]+?) Uhr, gibt es im Landkreis Biberach (?<cumulatedInfected3>\d+) bestätigte Coronafälle)|(?:(?<cumulatedInfected4>\d+) bestätigte Coronafälle \(Stand (?<updateDate4>[^<]+?) Uhr\)|Im Landkreis Biberach wurden aktuell \(Stand:? (?<updateDate5>[^<]+?) Uhr\) (?<cumulatedInfected5>\d+) Personen positiv auf das Coronavirus getestet|Gesundheitsamt Biberach meldet aktuell (?<cumulatedInfected6>\d+) positiv getestete Personen auf das Coronavirus \(Stand (?<updateDate6>[^<]+?) Uhr\))/
         );
         return {
             NUTS: 'DE146',
-            updateDate: moment.tz(groups.updateDate1 || groups.updateDate2 || groups.updateDate3 || groups.updateDate4 || groups.updateDate5, 'DD. MMM, HH', 'de', 'Europe/Berlin').toISOString(),
-            cumulatedInfected: groups.cumulatedInfected1 || groups.cumulatedInfected2 || groups.cumulatedInfected3 || groups.cumulatedInfected4 || groups.cumulatedInfected5
+            updateDate: moment.tz(groups.updateDate1 || groups.updateDate2 || groups.updateDate3 || groups.updateDate4 || groups.updateDate5 || groups.updateDate6, 'DD. MMM, HH', 'de', 'Europe/Berlin').toISOString(),
+            cumulatedInfected: groups.cumulatedInfected1 || groups.cumulatedInfected2 || groups.cumulatedInfected3 || groups.cumulatedInfected4 || groups.cumulatedInfected5 || groups.cumulatedInfected6
         };
     }
 }
