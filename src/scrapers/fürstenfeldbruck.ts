df609a2dc69231049a477a1d1657ae99cbfd1118
Augustus Kling <augustus.kling@gmail.com>
2020-04-08 10:40:30 +0200

    Fix Fürstenfeldbruck

--- src/scrapers/fürstenfeldbruck.ts
+++ src/scrapers/fürstenfeldbruck.ts
@@ -4,8 +4,8 @@ import * as moment from 'moment-timezone';
 class ScraperImpl extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
-            'https://www.lra-ffb.de/aktuelles/aktuelles/news/corona-epidemie-im-landkreis-fuerstenfeldbruck-neue-entwicklungen/',
-            /Aktuelle Zahlen[^]+?Stand (?<updateDate>\d+\.\d+\.\d\d\d\d)[^]+?Im Landkreis Fürstenfeldbruck sind dem Gesundheitsamt (?<cumulatedInfected>\d+) Personen mit Wohnsitz im Landkreis Fürstenfeldbruck als positiv getestet gemeldet/
+            'https://www.lra-ffb.de/amt-service/veroeffentlichungen/pressemitteilungen/',
+            /Mit Stand (?<updateDate>\d+\.\d+\.\d\d\d\d) gibt es insgesamt (?<cumulatedInfected>\d+) Corona Infizierte mit Wohnsitz im Landkreis Fürstenfeldbruck./
         );
         return {
             ...groups,
