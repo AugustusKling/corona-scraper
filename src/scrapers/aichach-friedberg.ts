f8fd8b9e37b815924348d1ff0e60f5bfefad9d49
Augustus Kling <augustus.kling@gmail.com>
2020-04-12 13:42:20 +0200

    Fix Aichach-Friedberg

--- src/scrapers/aichach-friedberg.ts
+++ src/scrapers/aichach-friedberg.ts
@@ -5,7 +5,7 @@ class ScraperImpl extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
             'https://lra-aic-fdb.de/landratsamt/fachbereiche/abteilung-3-oeffentliche-sicherheit/gesundheitsamt/informationen-zum-coronavirus',
-            /\w+,\s*(?<updateDate>\d\d\.\d\d\.\d\d\d\d)<\/p>(?:[^](?!\d\d\.\d\d\.\d\d\d\d<\/p>))+?Im Landkreis Aichach-Friedberg sind dem Gesundheitsamt aktuell <strong>(?<currentlyInfected>\d+)<\/strong> <strong>mit Corona infizierte Personen bekannt:<\/strong> (?<cumulatedInfected>\d+) wurden seit Beginn mit positivem Testergebnis registriert, (?<cumulatedRecovered>\d+) davon sind bereits wieder gesund. In den <strong>Kliniken an der Paar<\/strong> werden derzeit (?<cumulatedHospitalized>\d+) Patienten mit Covid-19 stationär behandelt/
+            /\w+,\s*(?<updateDate>\d\d\.\d\d\.\d\d\d\d)<\/p>(?:[^](?!\d\d\.\d\d\.\d\d\d\d<\/p>))+?Im Landkreis Aichach-Friedberg sind dem Gesundheitsamt <strong>aktuell (?<currentlyInfected>\d+)<\/strong> <strong>mit Corona infizierte Personen bekannt.<\/strong> Seit Beginn wurden Stand \w+ (?<cumulatedInfected>\d+) Personen mit positivem Testergebnis registriert, (?<cumulatedRecovered>\d+) davon sind bereits wieder gesund und aus der Quarantäne entlassen.(?:.(?!<\/p>))+?In den <strong>Kliniken an der Paar<\/strong> werden derzeit (?<cumulatedHospitalized>\d+) Patienten mit Covid-19 stationär behandelt/
         );
             return {
             ...groups,
