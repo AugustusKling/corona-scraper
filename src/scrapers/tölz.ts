1df86ca212e534141b5399eb919db198c33e2a35
Augustus Kling <augustus.kling@gmail.com>
2020-04-12 13:49:58 +0200

    Fix Tölz

--- src/scrapers/tölz.ts
+++ src/scrapers/tölz.ts
@@ -5,12 +5,12 @@ class ScraperImpl extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
             'https://www.lra-toelz.de/pressemitteilungen',
-            /<li>Pressemitteilung: (?<updateDate>\d\d\.\d\d\.\d\d\d\d)<\/li>(?:[^](?!<p class="teaser__download">))+(?:(?<cumulatedInfected1>\d+) infizierte Personen[^<]+|Zahl der Infizierten steigt auf (?<cumulatedInfected2>\d+).+?|(?<cumulatedInfected3>\d+) mit dem Coronavirus Infizierte.+?|Insgesamt haben sich nun (?<cumulatedInfected4>\d+) Personen im Landkreis mit dem Virus infiziert)/
+            /<li>Pressemitteilung: (?<updateDate>\d\d\.\d\d\.\d\d\d\d)<\/li>(?:[^](?!<p class="teaser__download">))+?(?:(?<cumulatedInfected1>\d+) infizierte Personen[^<]+|Zahl der Infizierten steigt auf (?<cumulatedInfected2>\d+).+?|(?<cumulatedInfected3>\d+) mit dem Coronavirus Infizierte.+?|Insgesamt haben sich nun (?<cumulatedInfected4>\d+) Personen im Landkreis mit dem Virus infiziert|(?<cumulatedInfected5>\d+) Corona-Infizierte im Landkreis)/
         );
         return {
             NUTS: 'DE216',
             updateDate: moment.tz(groups.updateDate, 'DD.MM.YYYY', 'de', 'Europe/Berlin').format('YYYY-MM-DD'),
-            cumulatedInfected: groups.cumulatedInfected1 || groups.cumulatedInfected2 || groups.cumulatedInfected3 || groups.cumulatedInfected4
+            cumulatedInfected: groups.cumulatedInfected1 || groups.cumulatedInfected2 || groups.cumulatedInfected3 || groups.cumulatedInfected4 || groups.cumulatedInfected5
         };
     }
 }
