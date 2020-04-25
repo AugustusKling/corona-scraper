6bdfb4ffda7c886f485bd49d512172c8333764dd
Augustus Kling <augustus.kling@gmail.com>
2020-04-25 13:39:23 +0200

    Fix Ravensburg

--- src/scrapers/ravensburg.ts
+++ src/scrapers/ravensburg.ts
@@ -1,14 +1,16 @@
 import { Scraper } from '../scraper';
+import * as moment from 'moment-timezone';
 
 class Ravensburg extends Scraper {
     public async get() {
         const { groups } = await this.downloadAndMatch(
             'https://www.rv.de/corona',
-            /(?<cumulatedInfected>\d+)\s+bekannte\s+Coronavirus-Infektionsfälle<\/strong><br>\s*davon sind <strong>(?<cumulatedRecoved>\d+) Personen wieder gesund/
+            /<strong>(?<cumulatedInfected>\d+)\s+bekannte\s+Coronavirus-Infektionsfälle \(Stand\s+(?<updateDate>\d+\d\.\d\d\.\d{4})\)<\/strong><br>\s*davon sind <strong>(?<cumulatedRecoved>\d+) Personen wieder gesund<\/strong>(?:[^.]+(?!<\/ul>))<span[^>]*>(?<cumulatedDeaths>\d+)\s*<span[^>]*>Personen\s+sind verstorben/
         );
         return {
             ...groups,
-            NUTS: 'DE148'
+            NUTS: 'DE148',
+            updateDate: moment.tz(groups.updateDate, 'DD.MM.YYYY', 'de', 'Europe/Berlin').format('YYYY-MM-DD')
         };
     }
 }
